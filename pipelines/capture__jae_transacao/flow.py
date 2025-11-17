45c6d437b81f6f64093706b5bda3d2ce4d6fe690
Rafael Pinheiro <rafael.c.pinheiro96@gmail.com>
2025-11-17 11:10:26 -0300

    teste is_docker

--- pipelines/capture__jae_transacao/flow.py
+++ pipelines/capture__jae_transacao/flow.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
-from prefect import flow
+from pathlib import Path
+
+from prefect import flow, task
 
 from pipelines.capture__jae_transacao import constants
 from pipelines.capture__jae_transacao.tasks import create_jae_general_extractor
 from pipelines.common.capture.default_capture.flow import (
     create_capture_flows_default_tasks,
 )
+
+
+@task
+def is_docker():
+    cgroup = Path("/proc/self/cgroup")
+    return Path("/.dockerenv").is_file() or (cgroup.is_file() and "docker" in cgroup.read_text())
 
 
 @flow(log_prints=True)
@@ -16,6 +24,7 @@ def capture__jae_transacao(
     recapture_days=2,
     recapture_timestamps=None,
 ):
+    print(is_docker())
     create_capture_flows_default_tasks(
         env=env,
         sources=[constants.TRANSACAO_SOURCE],
