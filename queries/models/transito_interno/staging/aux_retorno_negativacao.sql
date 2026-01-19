2f2f2d7e900829b659c82417deccdb9c9c058ba4
Guilherme Botelho <guilhermehcb@hotmail.com>
2026-01-19 11:42:05 -0300

    refactor: deduplicate `aux_retorno_negativacao`

--- queries/models/transito_interno/staging/aux_retorno_negativacao.sql
+++ queries/models/transito_interno/staging/aux_retorno_negativacao.sql
@@ -11,7 +11,12 @@
 
 with
     source_previnity as (
-        select data, safe_cast(data_autuacao as date) as data_autuacao, response, payload, "previnity" as fonte
+        select
+            data,
+            safe_cast(data_autuacao as date) as data_autuacao,
+            response,
+            payload,
+            "previnity" as fonte
         from {{ source("source_previnity", "retorno_negativacao") }}
         {% if is_incremental() %}
             where data = date('{{ var("date_range_end") }}')
@@ -25,19 +30,37 @@ with
             replace(payload, "'", '"') as payload_json,
             fonte
         from source_previnity
+    ),
+    unnested as (
+        select
+            data,
+            data_autuacao,
+            parse_datetime(
+                '%d/%m/%Y %H:%M:%S', json_value(r, '$.data')
+            ) as datetime_retorno,
+            json_value(r, '$.produtonome') as produtonome,
+            json_value(r, '$.produtoreferencia') as produtoreferencia,
+            json_value(r, '$.protocolo') as protocolo,
+            json_value(payload_json, '$.contrato') as contrato,
+            json_value(payload_json, '$.cpf') as cpf,
+            json_value(r, '$.resultado') as resultado,
+            fonte
+        from parsed, unnest(response_array) as r
     )
 select
     data,
     data_autuacao,
-    parse_datetime('%d/%m/%Y %H:%M:%S', json_value(r, '$.data')) as datetime_retorno,
-    json_value(r, '$.produtonome') as produtonome,
-    json_value(r, '$.produtoreferencia') as produtoreferencia,
-    json_value(r, '$.protocolo') as protocolo,
-    json_value(payload_json, '$.contrato') as contrato,
-    json_value(payload_json, '$.cpf') as cpf,
-    json_value(r, '$.resultado') as resultado,
+    datetime_retorno,
+    produtonome,
+    produtoreferencia,
+    protocolo,
+    contrato,
+    cpf,
+    resultado,
     fonte,
     current_datetime("America/Sao_Paulo") as datetime_ultima_atualizacao,
     "{{ var('version') }}" as versao,
     '{{ invocation_id }}' as id_execucao_dbt
-from parsed, unnest(response_array) as r
+from unnested
+qualify
+    row_number() over (partition by data, contrato order by datetime_retorno desc) = 1
