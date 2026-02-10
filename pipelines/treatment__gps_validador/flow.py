34f81658e2262864c8a7b2bc932a1079a042084f
Rafael Pinheiro <rafael.c.pinheiro96@gmail.com>
2026-02-10 10:20:07 -0300

    add handler_notify_failure

--- pipelines/treatment__gps_validador/flow.py
+++ pipelines/treatment__gps_validador/flow.py
@@ -11,10 +11,16 @@ from pipelines.common.treatment.default_treatment.flow import (
     create_materialization_flows_default_tasks,
 )
 from pipelines.common.treatment.default_treatment.utils import rename_treatment_flow_run
+from pipelines.common.utils.prefect import handler_notify_failure
 from pipelines.treatment__gps_validador import constants
 
 
-@flow(log_prints=True, flow_run_name=rename_treatment_flow_run)
+@flow(
+    log_prints=True,
+    flow_run_name=rename_treatment_flow_run,
+    on_failure=[handler_notify_failure(webhook="alertas_bilhetagem")],
+    on_crashed=[handler_notify_failure(webhook="alertas_bilhetagem")],
+)
 def treatment__gps_validador(  # noqa: PLR0913
     env=None,
     datetime_start=None,
