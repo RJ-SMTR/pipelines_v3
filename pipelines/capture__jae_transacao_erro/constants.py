1a921fd82da8086cf2dc74ea3c062c9b51075b28
Rafael Pinheiro <rafael.c.pinheiro96@gmail.com>
2026-02-11 14:00:28 -0300

    altera data inicial

--- pipelines/capture__jae_transacao_erro/constants.py
+++ pipelines/capture__jae_transacao_erro/constants.py
@@ -13,10 +13,9 @@ from pipelines.common.utils.gcp.bigquery import SourceTable
 JAE_TRANSACAO_ERRO_SOURCE = SourceTable(
     source_name=jae_constants.JAE_SOURCE_NAME,
     table_id=jae_constants.TRANSACAO_ERRO_TABLE_ID,
-    first_timestamp=datetime(2023, 7, 17, 0, 0, 0, tzinfo=ZoneInfo(smtr_constants.TIMEZONE)),
+    first_timestamp=datetime(2026, 1, 26, 23, 1, 0, tzinfo=ZoneInfo(smtr_constants.TIMEZONE)),
     flow_folder_name="capture__jae_transacao_erro",
     primary_keys=["id_transacao_recebida"],
     bucket_names=jae_constants.JAE_PRIVATE_BUCKET_NAMES,
     max_recaptures=60,
-    file_chunk_size=50000,
 )
