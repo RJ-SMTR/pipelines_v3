bb653c1626a2ac541cdb410440b505829ea3b9ea
Guilherme Botelho <guilhermehcb@hotmail.com>
2026-02-26 07:35:04 -0300

    fix: use date() and parse partition_id in view_pessoa_fisica_negativacao

--- queries/models/transito_interno/view_pessoa_fisica_negativacao.sql
+++ queries/models/transito_interno/view_pessoa_fisica_negativacao.sql
@@ -3,14 +3,17 @@
 {% set aux_autuacao_negativacao = ref("aux_autuacao_negativacao") %}
 {% if execute %}
     {% set partitions_query %}
-        select distinct concat("'", partition_id, "'") as partition_date
+        select distinct
+            concat(
+                "'", format_date('%Y-%m-%d', parse_date('%Y%m%d', partition_id)), "'"
+            ) as partition_date
         from `{{ aux_autuacao_negativacao.database }}.{{ aux_autuacao_negativacao.schema }}.INFORMATION_SCHEMA.PARTITIONS`
         where
             table_name = "{{ aux_autuacao_negativacao.identifier }}"
             and partition_id != "__NULL__"
-            and datetime(last_modified_time, "America/Sao_Paulo")
-                between datetime("{{ var('date_range_start') }}")
-                and (datetime("{{ var('date_range_end') }}"))
+            and date(last_modified_time, "America/Sao_Paulo")
+                between date("{{ var('date_range_start') }}")
+                and date("{{ var('date_range_end') }}")
     {% endset %}
     {% set partitions = run_query(partitions_query).columns[0].values() %}
 {% endif %}
