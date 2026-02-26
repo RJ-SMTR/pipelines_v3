684a31c951a60256d208c4d0f2136eba9aba9b94
hellcassius <caiorogerio.santos@gmail.com>
2026-02-26 09:48:58 -0300

    change error trigger

--- pipelines/test__raise_for_sentry/flow.py
+++ pipelines/test__raise_for_sentry/flow.py
@@ -13,4 +13,9 @@ from pipelines.common.utils.state_handlers import handler_post_sentry
 def test__raise_for_sentry() -> list[str]:
     print("Iniciando o flow test__raise_for_sentry")
     sleep(5)
-    raise(ValueError("Erro proposital para testar integração com Sentry"))
+    err = 1/0
+    # raise(ValueError("Erro proposital para testar integração com Sentry"))
+
+
+if __name__ == "__main__":
+    test__raise_for_sentry()
