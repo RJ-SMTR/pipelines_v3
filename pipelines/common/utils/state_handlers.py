b0c05e934b3f912b1baaecb111a7d29817769cf0
hellcassius <caiorogerio.santos@gmail.com>
2026-02-26 10:13:09 -0300

    fix get secret

--- pipelines/common/utils/state_handlers.py
+++ pipelines/common/utils/state_handlers.py
@@ -4,8 +4,9 @@ from pipelines.common.utils.secret import get_secret
 def handler_post_sentry(flow, flow_run, state):
 
     def handler(flow, flow_run, state):  # noqa: ARG001
-        sentry_dsn = get_secret("SENTRY_DSN")
+        sentry_dsn = get_secret("sentry", "dsn")
         environment = 'staging'
+        print('Inicializando sentry_sdk com DSN:', sentry_dsn)
         sentry_sdk.init(
             dsn=sentry_dsn,
             traces_sample_rate=0,
--- pipelines/test__raise_for_sentry/flow.py
+++ pipelines/test__raise_for_sentry/flow.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
-from prefect import flow
+from prefect import flow, task
 from time import sleep
 
 from pipelines.common.utils.state_handlers import handler_post_sentry
 
-
+@task
+def task_raises_exception():
+    print("Iniciando o flow test__raise_for_sentry")
+    sleep(5)
+    err = 1/0
 
 @flow(
         log_prints=True, 
         on_running=[handler_post_sentry]
 )
 def test__raise_for_sentry() -> list[str]:
-    print("Iniciando o flow test__raise_for_sentry")
-    sleep(5)
-    err = 1/0
+    task_raises_exception()
     # raise(ValueError("Erro proposital para testar integração com Sentry"))
 
 
