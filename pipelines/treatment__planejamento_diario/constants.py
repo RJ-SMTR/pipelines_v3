fdfdac4fc69d6fb37bcbae96ca71d742d222c9cb
Guilherme Botelho <guilhermehcb@hotmail.com>
2026-03-06 10:36:33 -0300

    Migração do flow de materialização do planejamento (#64)
    * feat: migrate planejamento_diario flow to Prefect 3.0
    
    * chore: update CHANGELOG with PR number #64
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * chore: copy missing dbt models for planejamento_diario selector
    
    * chore: suppress unused argument lint error in aux_segmento_shape
    
    * feat: add snapshot_viagem_planejada to planejamento_diario flow
    
    * fix: update snapshot_viagem_planejada reference to snapshot_viagem_planejada_planejamento
    
    * chore: register flow
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ pipelines/treatment__planejamento_diario/constants.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Valores constantes para materialização do selector planejamento_diario.
+"""
+
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+from pipelines.common import constants as smtr_constants
+from pipelines.common.treatment.default_treatment.utils import DBTSelector
+
+PLANEJAMENTO_DIARIO_SELECTOR = DBTSelector(
+    name="planejamento_diario",
+    initial_datetime=datetime(2024, 9, 1, 0, 0, 0, tzinfo=ZoneInfo(smtr_constants.TIMEZONE)),
+    flow_folder_name="treatment__planejamento_diario",
+)
+
+SNAPSHOT_PLANEJAMENTO_SELECTOR = DBTSelector(
+    name="snapshot_planejamento",
+    initial_datetime=datetime(2024, 9, 1, 0, 0, 0, tzinfo=ZoneInfo(smtr_constants.TIMEZONE)),
+    flow_folder_name="treatment__planejamento_diario",
+)
