54ff2ec1a6113a9cad91b50481e33abbd600bc9d
Rafael Pinheiro <rafael.c.pinheiro96@gmail.com>
2026-03-09 08:56:10 -0300

    .

--- queries/dev/run.py
+++ queries/dev/run.py
@@ -10,7 +10,6 @@ from queries.dev.utils import run_dbt_model
 # Veja os parâmetros disponíveis da função run_dbt_model em util.py
 
 run_dbt_model(
-    dataset_id="transacao_erro",
-    _vars={"date_range_start": "2026-03-05 00:00:00", "date_range_end": "2026-03-10 00:00:00"},
-    flags="--defer --state target-base --favor-state",
+    dataset_id="example",
+    table_id="my_first_dbt_model",
 )
