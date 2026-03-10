1c9bb757b7e8ad8bd149e93376be8720ac06f594
Guilherme Botelho <guilhermehcb@hotmail.com>
2026-03-10 12:44:36 -0300

    feat: add control__source_freshness flow

---
+++ pipelines/control__source_freshness/CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## [1.0.0] - 2026-03-10
+
+### Adicionado
+
+- Cria flow `control__source_freshness` (https://github.com/RJ-SMTR/pipelines_v3/pull/XXX)
---
+++ pipelines/control__source_freshness/constants.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Valores constantes para pipeline control__source_freshness
+"""
+
+DISCORD_WEBHOOK_KEY = "dataplex"
+DISCORD_MENTION_GROUP = "dados_smtr"
