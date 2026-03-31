46c7f7ef5bbb270b2415569da13da50d8e285d2e
adrianomsn <adrianoneto27@gmail.com>
2026-03-31 14:03:58 -0300

    Adiciona o selector de integracao no flow

--- pipelines/treatment__integracao/flow.py
+++ pipelines/treatment__integracao/flow.py
@@ -1,14 +1,23 @@
 # -*- coding: utf-8 -*-
+"""
+Flow de materialização das integrações
+
+Executa o selector DBT 'integracao' para materializar dados no BigQuery.
+
+DBT: 2026-03-31
+"""
+
 from prefect import flow
 
 from pipelines.common.treatment.default_treatment.flow import (
     create_materialization_flows_default_tasks,
 )
 from pipelines.common.treatment.default_treatment.utils import rename_treatment_flow_run
+from pipelines.treatment__integracao import constants
 
 
 @flow(log_prints=True, flow_run_name=rename_treatment_flow_run)
-def treatment__integracao(
+def treatment__integracao(  # noqa: PLR0913
     env=None,
     datetime_start=None,
     datetime_end=None,
@@ -18,7 +27,7 @@ def treatment__integracao(
 ):
     create_materialization_flows_default_tasks(
         env=env,
-        selectors=[],
+        selectors=[constants.INTEGRACAO_SELECTOR],
         datetime_start=datetime_start,
         datetime_end=datetime_end,
         flags=flags,
