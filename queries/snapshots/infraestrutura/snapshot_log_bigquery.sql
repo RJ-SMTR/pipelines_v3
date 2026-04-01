c161c6157d6c74be44f6d05041b3df2b6ae54752
Guilherme Botelho <guilhermehcb@hotmail.com>
2026-04-01 18:28:59 -0300

    feat: add partition filter to snapshots to avoid full scans on invalidate_hard_deletes

--- queries/snapshots/infraestrutura/snapshot_log_bigquery.sql
+++ queries/snapshots/infraestrutura/snapshot_log_bigquery.sql
@@ -1,5 +1,7 @@
 {% snapshot snapshot_log_bigquery %}
 
+    {%- set partition_filter = get_modified_partitions_filter(ref("log_bigquery")) -%}
+
     {{
         config(
             target_schema="infraestrutura_staging",
@@ -8,6 +10,7 @@
             updated_at="timestamp_ultima_atualizacao",
             invalidate_hard_deletes=True,
             partition_by={"field": "data", "data_type": "date", "granularity": "day"},
+            partition_filter=partition_filter,
         )
     }}
 
@@ -17,5 +20,6 @@
             datetime_ultima_atualizacao, "America/Sao_Paulo"
         ) as timestamp_ultima_atualizacao
     from {{ ref("log_bigquery") }}
+    where {{ partition_filter }}
 
 {% endsnapshot %}
