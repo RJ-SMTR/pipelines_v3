15c243c0f40203458c2dbb21086d5dc8f8dd28c1
Guilherme Botelho <guilhermehcb@hotmail.com>
2026-04-16 17:10:27 -0300

    feat: migrate capture__sppo_registros from Prefect 1.4 to 3.0

---
+++ pipelines/capture__sppo_registros/constants.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Valores constantes para captura de dados de GPS registros do SPPO
+"""
+
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+from pipelines.common import constants as smtr_constants
+from pipelines.common.capture.gps import constants as gps_constants
+from pipelines.common.utils.gcp.bigquery import SourceTable
+
+SPPO_REGISTROS_SOURCE = SourceTable(
+    source_name=gps_constants.SPPO_SOURCE_NAME,
+    table_id=gps_constants.REGISTROS_TABLE_ID,
+    first_timestamp=datetime(2026, 4, 16, 17, 0, 0, tzinfo=ZoneInfo(smtr_constants.TIMEZONE)),
+    flow_folder_name="capture__sppo_registros",
+    primary_keys=["id_veiculo", "datetime_servidor"],
+    pretreatment_reader_args={"dtype": "object", "convert_dates": False},
+)
