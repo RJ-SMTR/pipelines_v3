b9e97319a3114906441d5d01bf22da0027e8a68e
Duartejanaina <janainacduarte@gmail.com>
2026-04-30 16:12:09 -0300

    Ajusta constants

--- pipelines/treatment__cadastro_veiculo/constants.py
+++ pipelines/treatment__cadastro_veiculo/constants.py
@@ -17,5 +17,6 @@ CADASTRO_VEICULO_SELECTOR = DBTSelector(
 
 SNAPSHOT_CADASTRO_VEICULO_SELECTOR = DBTSelector(
     name="snapshot_cadastro_veiculo",
+    initial_datetime=datetime(2025, 6, 23, 6, 0, 0, tzinfo=ZoneInfo(smtr_constants.TIMEZONE)),
     flow_folder_name="treatment__cadastro_veiculo",
 )
