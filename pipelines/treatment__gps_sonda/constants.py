0b8c59649d4382e5287fcdb1cabc9a41185b5dbd
Guilherme Botelho <guilhermehcb@hotmail.com>
2026-05-06 15:49:26 -0300

    [HOTFIX] remove data_sources from GPS_SONDA_SELECTOR (#159)
    * refactor: remove data_sources from GPS_SONDA_SELECTOR configuration
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * refactor: remove data_sources from GPS_15_MINUTOS_SPPO_SELECTOR
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix: update first_timestamp to 2026-04-30 for infracao and licenciamento sources
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- pipelines/treatment__gps_sonda/constants.py
+++ pipelines/treatment__gps_sonda/constants.py
@@ -6,7 +6,6 @@ Valores constantes para materialização do selector gps Sonda (BRT)
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
-from pipelines.capture__sonda_registros import constants as sonda_constants
 from pipelines.common import constants as smtr_constants
 from pipelines.common.treatment.default_treatment.utils import DBTSelector
 
@@ -22,5 +21,4 @@ GPS_SONDA_SELECTOR = DBTSelector(
     flow_folder_name="treatment__gps_sonda",
     incremental_delay_hours=1,
     redis_key_suffix="sonda",
-    data_sources=[sonda_constants.SONDA_REGISTROS_SOURCE],
 )
