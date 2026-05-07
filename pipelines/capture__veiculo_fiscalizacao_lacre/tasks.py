89fd6529578c94f9e53e815f6f6f37f7277d869e
Duartejanaina <janainacduarte@gmail.com>
2026-05-07 18:18:38 -0300

    Ajusta a função

--- pipelines/capture__veiculo_fiscalizacao_lacre/tasks.py
+++ pipelines/capture__veiculo_fiscalizacao_lacre/tasks.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Tasks de captura dos dados de fiscalização de veículos"""
 
-from functools import partial
+from pathlib import Path
 
 from prefect import task
 from prefect.cache_policies import NO_CACHE
@@ -9,13 +9,18 @@ from prefect.cache_policies import NO_CACHE
 from pipelines.capture__veiculo_fiscalizacao_lacre import constants
 from pipelines.common.capture.default_capture.utils import SourceCaptureContext
 from pipelines.common.utils.extractors.gdrive import get_google_sheet_xlsx
+from pipelines.common.utils.fs import save_local_file
 
 
 @task(cache_policy=NO_CACHE)
-def create_veiculo_fiscalizacao_lacre_extractor(context: SourceCaptureContext):  # noqa: ARG001
+def create_veiculo_fiscalizacao_lacre_extractor(context: SourceCaptureContext):
     """Cria a extração da planilha de controle de lacre dos veículos"""
-    return partial(
-        get_google_sheet_xlsx,
+    df = get_google_sheet_xlsx(
         spread_sheet_id=constants.VEICULO_LACRE_SHEET_ID,
         sheet_name=constants.VEICULO_LACRE_SHEET_NAME,
     )
+
+    filepath = Path(context.raw_filepath).parent / "veiculo_fiscalizacao_lacre.csv"
+    save_local_file(filepath=str(filepath), filetype="csv", data=df)
+
+    return filepath
