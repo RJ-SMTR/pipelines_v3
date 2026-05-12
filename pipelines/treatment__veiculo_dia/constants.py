bfccbde8b3247b19aa0ca56d66b59f672175301f
Duartejanaina <janainacduarte@gmail.com>
2026-05-12 19:17:48 -0300

    Refactor constants.py: reorganize imports and update data sources for veiculo_dia treatment

--- pipelines/treatment__veiculo_dia/constants.py
+++ pipelines/treatment__veiculo_dia/constants.py
@@ -8,10 +8,20 @@ from zoneinfo import ZoneInfo
 
 from pipelines.common import constants as smtr_constants
 from pipelines.common.treatment.default_treatment.utils import DBTSelector, DBTTest
+from pipelines.treatment__cadastro_veiculo import constants as cadastro_veiculo
+from pipelines.treatment__monitoramento_veiculo import (
+    constants as monitoramento_veiculo_constants,
+)
+from pipelines.capture__veiculo_fiscalizacao_lacre import (
+    constants as veiculo_fiscalizacao_lacre_constants,
+)
+
 
 VEICULO_DIA_CHECKS_LIST = {
     "veiculo_dia": {
-        "not_null": {"description": "Todos os valores da coluna `{column_name}` não nulos"},
+        "not_null": {
+            "description": "Todos os valores da coluna `{column_name}` não nulos"
+        },
         "dbt_utils.unique_combination_of_columns__data_id_veiculo__veiculo_dia": {
             "description": "Todos os registros são únicos"
         },
@@ -32,15 +42,23 @@ VEICULO_DIA_TEST = DBTTest(
 
 VEICULO_DIA_SELECTOR = DBTSelector(
     name="veiculo_dia",
-    initial_datetime=datetime(2025, 6, 23, 0, 0, 0, tzinfo=ZoneInfo(smtr_constants.TIMEZONE)),
+    initial_datetime=datetime(
+        2025, 6, 23, 0, 0, 0, tzinfo=ZoneInfo(smtr_constants.TIMEZONE)
+    ),
     incremental_delay_hours=24 * 7,
     flow_folder_name="treatment__veiculo_dia",
     post_test=VEICULO_DIA_TEST,
-    data_sources=["cadastro_veiculo", "monitoramento_veiculo", "capture__veiculo_sppo_registro_agente_verao"],
+    data_sources=[
+        cadastro_veiculo.CADASTRO_VEICULO_SELECTOR,
+        monitoramento_veiculo_constants.MONITORAMENTO_VEICULO_SELECTOR,
+        veiculo_fiscalizacao_lacre_constants.VEICULO_LACRE_SOURCE,
+    ],
 )
 
 SNAPSHOT_VEICULO_DIA_SELECTOR = DBTSelector(
     name="snapshot_veiculo_dia",
-    initial_datetime=datetime(2025, 6, 23, 0, 0, 0, tzinfo=ZoneInfo(smtr_constants.TIMEZONE)),
+    initial_datetime=datetime(
+        2025, 6, 23, 0, 0, 0, tzinfo=ZoneInfo(smtr_constants.TIMEZONE)
+    ),
     flow_folder_name="treatment__veiculo_dia",
 )
