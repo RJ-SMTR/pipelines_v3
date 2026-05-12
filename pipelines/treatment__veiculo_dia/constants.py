0b2671907f704ba19ab68db41ec4ea636ed65033
Duartejanaina <janainacduarte@gmail.com>
2026-05-12 20:25:12 -0300

    Fix flow folder name for cadastro veiculo selector in constants.py

--- pipelines/treatment__veiculo_dia/constants.py
+++ pipelines/treatment__veiculo_dia/constants.py
@@ -42,7 +42,7 @@ CADASTO_VEICULO_DELAY_HOURS = 168
 WAIT_CADASTRO_VEICULO_SELECTOR = DBTSelector(
     name="cadastro_veiculo",
     initial_datetime=datetime(2025, 6, 23, 6, 0, 0, tzinfo=ZoneInfo(smtr_constants.TIMEZONE)),
-    flow_folder_name="tratment__cadastro_veiculo",
+    flow_folder_name="treatment__cadastro_veiculo",
     incremental_delay_hours=-CADASTO_VEICULO_DELAY_HOURS),
 
 VEICULO_DIA_TEST = DBTTest(
