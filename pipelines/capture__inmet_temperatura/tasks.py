15d09f57a6c65aa637a411ad28722c164fb6c561
Duartejanaina <janainacduarte@gmail.com>
2026-05-13 17:39:11 -0300

    refactor: update create_temperatura_extractor to use SourceCaptureContext

--- pipelines/capture__inmet_temperatura/tasks.py
+++ pipelines/capture__inmet_temperatura/tasks.py
@@ -1,27 +1,25 @@
 # -*- coding: utf-8 -*-
 """Tasks de captura dos dados de temperatura do INMET"""
 
-from datetime import datetime, timedelta
+from datetime import timedelta
 from functools import partial
 
 from prefect import task
+from prefect.cache_policies import NO_CACHE
 
 from pipelines.capture__inmet_temperatura import constants
+from pipelines.common.capture.default_capture.utils import SourceCaptureContext
 from pipelines.common.utils.extractors.api import get_raw_api_list
-from pipelines.common.utils.gcp.bigquery import SourceTable
 from pipelines.common.utils.secret import get_env_secret
 
 
-@task(log_prints=True)
-def create_temperatura_extractor(
-    _source: SourceTable,
-    timestamp: datetime,
-):
+@task(cache_policy=NO_CACHE)
+def create_temperatura_extractor(context: SourceCaptureContext):
     """Cria a extração de dados da api do INMET"""
 
-    start = timestamp - timedelta(days=1)
+    start = context.timestamp - timedelta(days=1)
     data_inicio = start.strftime("%Y-%m-%d")
-    data_fim = timestamp.strftime("%Y-%m-%d")
+    data_fim = context.timestamp.strftime("%Y-%m-%d")
 
     key = get_env_secret(constants.INMET_SECRET_PATH)
 
@@ -30,4 +28,4 @@ def create_temperatura_extractor(
         url = f"{constants.INMET_BASE_URL}/{data_inicio}/{data_fim}/{estacao}/{key}"
         url_list.append(url)
 
-    return partial(get_raw_api_list, url=url_list)
+    return partial(get_raw_api_list, url=url_list, raw_filepath=context.raw_filepath)
