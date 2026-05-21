7ba6a26a595b3d2d85ed1b26fedfe9f6965b405e
Guilherme Botelho <guilhermehcb@hotmail.com>
2026-05-20 21:21:06 -0300

    fix: correct logic

--- queries/tests/test_jae_captura_subsidio.sql
+++ queries/tests/test_jae_captura_subsidio.sql
@@ -1,15 +1,7 @@
 with
-    verificacao as (
-        select
-            timestamp_captura,
-            min(indicador_captura_correta) as indicador_captura_correta
-        from {{ source("source_jae", "resultado_verificacao_captura_jae") }}
-        where
-            data between date("{{ var('date_range_start') }}") and date(
-                "{{ var('date_range_end') }}"
-            )
-            and table_id in ('transacao', 'transacao_riocard', 'gps_validador')
-        group by 1
+    table_ids as (
+        select table_id
+        from unnest(['transacao', 'transacao_riocard', 'gps_validador']) as table_id
     ),
     ts as (
         select datetime(timestamp_captura, "America/Sao_Paulo") as timestamp_captura
@@ -22,17 +14,24 @@ with
                 )
             ) timestamp_captura
     ),
-    ts_filtrado as (
-        select ts.timestamp_captura
-        from ts
-        left join verificacao using (timestamp_captura)
-        qualify
-            sum(ifnull(cast(not indicador_captura_correta as integer), 1)) over (
-                order by ts.timestamp_captura
+    ts_table as (select * from ts cross join table_ids),
+    verificacao as (
+        select timestamp_captura, table_id, indicador_captura_correta
+        from {{ source("source_jae", "resultado_verificacao_captura_jae") }}
+        where
+            data between date("{{ var('date_range_start') }}") and date(
+                "{{ var('date_range_end') }}"
             )
-            = 0
-    ),
-    datetime_confiavel as (select max(timestamp_captura) as dt from ts_filtrado)
-select dt as datetime_confiavel
-from datetime_confiavel
-where dt is null or dt < datetime("{{ var('date_range_end') }}")
+            and table_id in ('transacao', 'transacao_riocard', 'gps_validador')
+    )
+select
+    timestamp_captura,
+    table_id,
+    case
+        when verificacao.timestamp_captura is null
+        then 'Sem registro'
+        else 'Captura divergente'
+    end as motivo
+from ts_table
+left join verificacao using (timestamp_captura, table_id)
+where ifnull(not verificacao.indicador_captura_correta, true)
