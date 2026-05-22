0faa983c07596a5c608e524f5cb9c6252fc89aea
Guilherme Botelho <guilhermehcb@hotmail.com>
2026-05-22 20:10:23 -0300

    feat: add require_partition_filter to autuacao model configuration

--- queries/models/transito/autuacao.sql
+++ queries/models/transito/autuacao.sql
@@ -4,6 +4,7 @@
         partition_by={"field": "data", "data_type": "date", "granularity": "day"},
         unique_key=["id_autuacao"],
         incremental_strategy="insert_overwrite",
+        require_partition_filter=true,
     )
 }}
 
