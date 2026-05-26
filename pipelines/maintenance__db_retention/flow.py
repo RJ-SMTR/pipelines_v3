0c88dfdd35c536f77a39b2a5d040955e543ef0d5
Caio <caiorogerio.santos@gmail.com>
2026-05-26 13:59:23 -0300

    adjust prefect lib version

--- pipelines/maintenance__db_retention/flow.py
+++ pipelines/maintenance__db_retention/flow.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import asyncio
 from os import getenv
 
 from tasks import delete_old_flow_runs, vacuum_tables, vacuum_index_bloat
@@ -12,3 +13,7 @@ async def maintenance__db_retention():
     await delete_old_flow_runs(days_to_keep=15, batch_size=200)
     await vacuum_tables(db_url=db_url, bloat_threshold=30.0)
     await vacuum_index_bloat(db_url=db_url, bloat_threshold=30.0)
+
+
+if __name__ == "__main__":
+    asyncio.run(maintenance__db_retention())
