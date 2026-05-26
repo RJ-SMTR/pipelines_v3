b4156d8f630cb9c6487842f26503ca6fc5a4a5d2
Caio <caiorogerio.santos@gmail.com>
2026-05-26 14:54:21 -0300

    force api url

--- pipelines/maintenance__db_retention/flow.py
+++ pipelines/maintenance__db_retention/flow.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import asyncio
-from os import getenv
+from os import getenv, environ
 
 from tasks import delete_old_flow_runs, vacuum_index_bloat, vacuum_tables
 
@@ -10,6 +10,7 @@ from pipelines.common.utils.prefect import flow
 @flow(log_prints=True)
 async def maintenance__db_retention():
     db_url = getenv("PREFECT_DB_URL")
+    environ["PREFECT_API_URL"] = "https://10.128.0.2/api"
     await delete_old_flow_runs(days_to_keep=15, batch_size=200)
     await vacuum_tables(db_url=db_url, bloat_threshold=30.0)
     await vacuum_index_bloat(db_url=db_url, bloat_threshold=30.0)
