77bc74417c42eaa3773443f2cbcfb4c0c4b1dbba
Caio <caiorogerio.santos@gmail.com>
2026-05-27 14:21:58 -0300

    remove force api

--- pipelines/maintenance__db_retention/flow.py
+++ pipelines/maintenance__db_retention/flow.py
@@ -10,7 +10,6 @@ from pipelines.common.utils.prefect import flow
 @flow(log_prints=True)
 async def maintenance__db_retention():
     db_url = getenv("PREFECT_DB_URL")
-    environ["PREFECT_API_URL"] = "https://10.128.0.2/api"
     await delete_old_flow_runs(days_to_keep=15, batch_size=200)
     await vacuum_tables(db_url=db_url, bloat_threshold=30.0)
     await vacuum_index_bloat(db_url=db_url, bloat_threshold=30.0)
