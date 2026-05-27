73b82a3915de5e5ee0c62f8e4b330d1b90770dff
Caio <caiorogerio.santos@gmail.com>
2026-05-27 16:09:33 -0300

    add paramaters to flow declaration

--- pipelines/maintenance__db_retention/flow.py
+++ pipelines/maintenance__db_retention/flow.py
@@ -8,9 +8,11 @@ from pipelines.common.utils.prefect import flow
 
 
 @flow(log_prints=True)
-async def maintenance__db_retention():
+async def maintenance__db_retention(
+    days_to_keep: int = 25, batch_size: int = 100, bloat_threshold: float = 30.0
+):
     db_url = getenv("PREFECT_DB_URL")
-    await delete_old_flow_runs(days_to_keep=15, batch_size=200)
+    await delete_old_flow_runs(days_to_keep=15, batch_size=100)
     await vacuum_tables(db_url=db_url, bloat_threshold=30.0)
     await vacuum_index_bloat(db_url=db_url, bloat_threshold=30.0)
 
