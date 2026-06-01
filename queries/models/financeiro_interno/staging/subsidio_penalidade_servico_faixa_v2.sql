aae8de511c24490ffca61d66e901a41e358a56ca
Rafael Pinheiro <rafael.c.pinheiro96@gmail.com>
2026-06-01 10:08:10 -0300

    cria modelo aux_subsidio_penalidade_servico_faixa_excecao

--- queries/models/financeiro_interno/staging/subsidio_penalidade_servico_faixa_v2.sql
+++ queries/models/financeiro_interno/staging/subsidio_penalidade_servico_faixa_v2.sql
@@ -47,33 +47,74 @@ with
             ifnull(- valor, 0) as valor_penalidade
         from {{ ref("valor_tipo_penalidade") }}
     -- from `rj-smtr.dashboard_subsidio_sppo.valor_tipo_penalidade`
+    ),
+    subsidio_valor_penalidade as (
+        select
+            s.data,
+            s.tipo_dia,
+            s.consorcio,
+            s.servico,
+            s.sentido,
+            faixa_horaria_inicio,
+            faixa_horaria_fim,
+            safe_cast(coalesce(pe.valor_penalidade, 0) as numeric) as valor_penalidade
+        from subsidio_dia as s
+        left join
+            penalidade as pe
+            on s.data between pe.data_inicio and pe.data_fim
+            and s.min_pof >= pe.perc_km_inferior
+            and s.min_pof < pe.perc_km_superior
+    ),
+    subsidio_valor_penalidade_ajustado as (
+        select
+            v.* except (valor_penalidade),
+            ifnull(e.valor_penalidade, v.valor_penalidade) as valor_penalidade,
+            e.data_inicio as data_inicio_excecao,
+            e.faixa_horaria_inicio as faixa_horaria_inicio_excecao,
+            e.faixa_horaria_fim as faixa_horaria_fim_excecao,
+            e.servico as servico_excecao
+        from subsidio_valor_penalidade v
+        left join
+            {{ ref("aux_subsidio_penalidade_servico_faixa_excecao") }} e
+            on v.data between e.data_inicio and e.data_fim
+            and (
+                v.faixa_horaria_inicio >= e.faixa_horaria_inicio
+                or e.faixa_horaria_inicio is null
+            )
+            and (
+                v.faixa_horaria_fim <= e.faixa_horaria_fim
+                or e.faixa_horaria_fim is null
+            )
+            and (v.servico = e.servico or e.servico is null)
+    ),
+    subsidio_valor_penalidade_ajustado_deduplicado as (
+        select
+            * except (
+                data_inicio_excecao,
+                faixa_horaria_inicio_excecao,
+                faixa_horaria_fim_excecao,
+                servico_excecao
+            )
+        from subsidio_valor_penalidade_ajustado
+        qualify
+            row_number() over (
+                partition by
+                    data,
+                    consorcio,
+                    servico,
+                    sentido,
+                    faixa_horaria_inicio,
+                    faixa_horaria_fim
+                order by
+                    servico_excecao desc,
+                    faixa_horaria_inicio_excecao desc,
+                    faixa_horaria_fim_excecao desc,
+                    data_inicio_excecao desc
+            )
+            = 1
     )
 select
-    s.data,
-    s.tipo_dia,
-    s.consorcio,
-    s.servico,
-    s.sentido,
-    faixa_horaria_inicio,
-    faixa_horaria_fim,
-    case
-        when
-            (
-                s.servico
-                in ("201", "202", "006", "133", "507", "426", "607", "711", "416")
-                and s.data = "2026-03-18"
-            )
-            or (s.servico in ("133", "607", "711") and s.data = "2026-03-19")
-            or (s.servico in ("133", "607") and s.data = "2026-03-20")
-        then 0  -- Processo SEI_000301.005390_2026_67
-        else safe_cast(coalesce(pe.valor_penalidade, 0) as numeric)
-    end as valor_penalidade,
+    *,
     '{{ var("version") }}' as versao,
     current_datetime("America/Sao_Paulo") as datetime_ultima_atualizacao
-
-from subsidio_dia as s
-left join
-    penalidade as pe
-    on s.data between pe.data_inicio and pe.data_fim
-    and s.min_pof >= pe.perc_km_inferior
-    and s.min_pof < pe.perc_km_superior
+from subsidio_valor_penalidade_ajustado_deduplicado
