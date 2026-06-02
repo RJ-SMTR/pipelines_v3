6aecc4a10303c8ff122e78380e7da0d8e420bdf5
Duartejanaina <janainacduarte@gmail.com>
2026-06-02 17:18:08 -0300

    ajusta conforme revisão

--- queries/models/projeto_subsidio_sppo/aux_viagem_circular.sql
+++ queries/models/projeto_subsidio_sppo/aux_viagem_circular.sql
@@ -3,17 +3,8 @@
 with
     ida_volta_circular as (
         select t.*
-        from
-            (
-                select *
-                from {{ ref("aux_ida_volta_circular_v1") }}
-                where data < date("{{ var('DATA_SUBSIDIO_V24_INICIO') }}")
-                full outer union all by name
-                select *
-                from {{ ref("aux_ida_volta_circular_v2") }}
-                where data >= date("{{ var('DATA_SUBSIDIO_V24_INICIO') }}")
 
-            ) t
+                from {{ ref("aux_ida_volta_circular") }} t
         where
             flag_proximo_volta = true
             and sentido_shape = "I"
--- queries/models/projeto_subsidio_sppo/staging/aux_ida_volta_circular_v2.sql
+++ queries/models/projeto_subsidio_sppo/staging/aux_ida_volta_circular_v2.sql
@@ -7,40 +7,20 @@
 select
     *,
     lead(datetime_partida) over (
-        partition by id_veiculo, servico_realizado
-        order by
-            id_veiculo,
-            servico_realizado,
-            datetime_partida,
-            sentido_shape,
-            shape_id_planejado
+        partition by id_veiculo, servico_realizado, shape_id_planejado
+        order by id_veiculo, servico_realizado, datetime_partida, sentido_shape
     ) as datetime_partida_volta,
     lead(datetime_chegada) over (
-        partition by id_veiculo, servico_realizado
-        order by
-            id_veiculo,
-            servico_realizado,
-            datetime_partida,
-            sentido_shape,
-            shape_id_planejado
+        partition by id_veiculo, servico_realizado, shape_id_planejado
+        order by id_veiculo, servico_realizado, datetime_partida, sentido_shape,
     ) as datetime_chegada_volta,
     lead(shape_id) over (
-        partition by id_veiculo, servico_realizado
-        order by
-            id_veiculo,
-            servico_realizado,
-            datetime_partida,
-            sentido_shape,
-            shape_id_planejado
+        partition by id_veiculo, servico_realizado, shape_id_planejado
+        order by id_veiculo, servico_realizado, datetime_partida, sentido_shape,
     ) as shape_id_volta,
     lead(sentido_shape) over (
-        partition by id_veiculo, servico_realizado
-        order by
-            id_veiculo,
-            servico_realizado,
-            datetime_partida,
-            sentido_shape,
-            shape_id_planejado
+        partition by id_veiculo, servico_realizado, shape_id_planejado
+        order by id_veiculo, servico_realizado, datetime_partida, sentido_shape,
     )
     = "V" as flag_proximo_volta  -- possui volta
 from {{ ref("aux_viagem_inicio_fim") }} v
