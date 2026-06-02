b0b2baa895454c35df0e45230bd463da13ea8158
Rafael Carvalho Pinheiro <74972217+pixuimpou@users.noreply.github.com>
2026-06-02 12:30:23 -0300

    Cria modelos com exceções das regras das tabelas `subsidio_penalidade_servico_faixa` e `viagens_remuneradas` (#220)
    * cria modelo de excecoes para a viagem remunerada
    
    * cria modelo aux_subsidio_penalidade_servico_faixa_excecao
    
    * add aux_viagem_remunerada_excecao e aux_subsidio_penalidade_servico_faixa_excecao
    
    * pr 220
    
    * add priority
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ queries/models/financeiro_interno/staging/aux_subsidio_penalidade_servico_faixa_excecao.sql
@@ -0,0 +1,62 @@
+{{
+    config(
+        materialized="view",
+    )
+}}
+
+
+with
+    dados_manuais as (
+        select *
+        from
+            unnest(
+                [
+                    struct(  -- Processo SEI_000301.005390_2026_67
+                        '2026-03-18' as data_inicio,
+                        '2026-03-18' as data_fim,
+                        cast(null as string) as faixa_horaria_inicio,
+                        cast(null as string) as faixa_horaria_fim,
+                        [
+                            "201",
+                            "202",
+                            "006",
+                            "133",
+                            "507",
+                            "426",
+                            "607",
+                            "711",
+                            "416"
+                        ] as servicos,
+                        0 as valor_penalidade
+                    ),
+                    struct(  -- Processo SEI_000301.005390_2026_67
+                        '2026-03-19' as data_inicio,
+                        '2026-03-19' as data_fim,
+                        cast(null as string) as faixa_horaria_inicio,
+                        cast(null as string) as faixa_horaria_fim,
+                        ["133", "607", "711"] as servicos,
+                        0 as valor_penalidade
+                    ),
+                    struct(  -- Processo SEI_000301.005390_2026_67
+                        '2026-03-20' as data_inicio,
+                        '2026-03-20' as data_fim,
+                        cast(null as string) as faixa_horaria_inicio,
+                        cast(null as string) as faixa_horaria_fim,
+                        ["133", "607"] as servicos,
+                        0 as valor_penalidade
+                    )
+                ]
+            )
+        with
+        offset as priority
+    )
+select
+    date(data_inicio) as data_inicio,
+    date(data_fim) as data_fim,
+    datetime(faixa_horaria_inicio) as faixa_horaria_inicio,
+    datetime(faixa_horaria_fim) as faixa_horaria_fim,
+    servico,
+    valor_penalidade,
+    priority
+from dados_manuais
+left join unnest(servicos) as servico
