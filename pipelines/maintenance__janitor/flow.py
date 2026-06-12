0a7c57604ef11c0519bcf765de7288da2055b407
Caio <caiorogerio.santos@gmail.com>
2026-06-12 14:30:27 -0300

    remove unused import for merging

--- pipelines/maintenance__janitor/flow.py
+++ pipelines/maintenance__janitor/flow.py
@@ -4,8 +4,6 @@ from tasks import delete_stale_pending_runs
 
 from pipelines.common.utils.prefect import flow
 
-import pipelines.common as common
-
 @flow(log_prints=True)
 async def maintenance__janitor():
     await delete_stale_pending_runs(threshold_hours=1, batch_size=200)
