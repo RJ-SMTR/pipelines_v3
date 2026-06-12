2ac7b840fd2def57fea4c4303567f849598baf16
Caio <caiorogerio.santos@gmail.com>
2026-06-12 14:19:25 -0300

    trigger cd

--- pipelines/maintenance__janitor/flow.py
+++ pipelines/maintenance__janitor/flow.py
@@ -4,6 +4,7 @@ from tasks import delete_stale_pending_runs
 
 from pipelines.common.utils.prefect import flow
 
+import pipelines.common as common
 
 @flow(log_prints=True)
 async def maintenance__janitor():
