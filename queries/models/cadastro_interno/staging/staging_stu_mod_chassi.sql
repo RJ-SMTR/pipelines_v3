d797b5def96910fb4b88f3ca2b998184bfd0128d
Guilherme Botelho <guilhermehcb@hotmail.com>
2026-07-01 23:47:44 -0300

    fix: parse STU chassis model key from source column

--- queries/models/cadastro_interno/staging/staging_stu_mod_chassi.sql
+++ queries/models/cadastro_interno/staging/staging_stu_mod_chassi.sql
@@ -3,9 +3,7 @@
 select
     data,
     safe_cast(
-        replace(
-            safe_cast(json_value(content, '$.cod_mod_chassi') as string), '.0', ''
-        ) as string
+        replace(safe_cast(cod_mod_chassi as string), '.0', '') as string
     ) as id_modelo_chassi,
     safe_cast(cod_fab_chassi as string) as id_fabricante,
     safe_cast(json_value(content, '$.des_mod_chassi') as string) as descricao,
