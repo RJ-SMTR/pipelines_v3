fd2741365256bef2b147ab2809594fcdf5f0b901
Rafael Carvalho Pinheiro <74972217+pixuimpou@users.noreply.github.com>
2026-07-06 11:29:17 -0300

    Migra tratamento timestamps divergentes das capturas da Jae (#348)
    * cria flow treatment__jae_timestamps_divergentes
    
    * ajustes
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ pipelines/treatment__jae_timestamps_divergentes/flow.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+from typing import Optional
+
+from prefect import runtime
+
+from pipelines.common.capture.jae import constants as jae_constants
+from pipelines.common.tasks import get_run_env, initialize_sentry, run_subflow, setup_environment
+from pipelines.common.treatment.default_treatment.utils import rename_treatment_flow_run
+from pipelines.common.utils.prefect import flow
+from pipelines.control__jae_verificacao_captura.flow import control__jae_verificacao_captura
+from pipelines.treatment__jae_timestamps_divergentes import constants
+from pipelines.treatment__jae_timestamps_divergentes.tasks import (
+    create_materialization_subflows_params,
+    create_recapture_subflows_params,
+    create_transacao_valor_ordem_params,
+    create_verificacao_captura_params,
+    get_gaps_from_result_table,
+    run_updates,
+)
+from pipelines.treatment__passageiro_hora.flow import treatment__passageiro_hora
+from pipelines.treatment__transacao_valor_ordem.flow import treatment__transacao_valor_ordem
+
+
+@flow(log_prints=True, flow_run_name=rename_treatment_flow_run)
+async def treatment__jae_timestamps_divergentes(
+    env: Optional[str] = None,
+    datetime_start: Optional[str] = None,
+    datetime_end: Optional[str] = None,
+):
+    deployment_name = runtime.deployment.name
+
+    env = get_run_env(
+        env=env,
+        deployment_name=deployment_name,
+    )
+
+    setup_enviroment = setup_environment(env=env)
+
+    sentry = initialize_sentry(env=env)
+
+    tables = constants.CAPTURE_GAP_TABLES
+
+    gaps = get_gaps_from_result_table(
+        env=env,
+        table_ids=tables.keys(),
+        timestamp_start=datetime_start,
+        timestamp_end=datetime_end,
+        wait_for=[setup_enviroment, sentry],
+    )
+
+    subflow_capture_params = create_recapture_subflows_params(gaps=gaps)
+
+    for param in subflow_capture_params:
+        await run_subflow(env=env, flow=param["flow"], parameters=param["params"])
+
+    subflow_materialization_params = create_materialization_subflows_params(gaps=gaps)
+
+    for param in subflow_materialization_params:
+        await run_subflow(env=env, flow=param["flow"], parameters=param["params"])
+
+    sql_treatment = run_updates(env=env, gaps=gaps)
+
+    if (
+        gaps[jae_constants.TRANSACAO_TABLE_ID]["flag_has_gaps"]
+        or gaps[jae_constants.TRANSACAO_RIOCARD_TABLE_ID]["flag_has_gaps"]
+        or gaps[jae_constants.CLIENTE_TABLE_ID]["flag_has_gaps"]
+        or gaps[jae_constants.GRATUIDADE_TABLE_ID]["flag_has_gaps"]
+        or gaps[jae_constants.ESTUDANTE_TABLE_ID]["flag_has_gaps"]
+        or gaps[jae_constants.LAUDO_PCD_TABLE_ID]["flag_has_gaps"]
+    ):
+        await run_subflow(env=env, flow=treatment__passageiro_hora, wait_for=[sql_treatment])
+
+    run_transacao_valor_ordem, params = create_transacao_valor_ordem_params(gaps=gaps)
+
+    if run_transacao_valor_ordem:
+        await run_subflow(
+            env=env,
+            flow=treatment__transacao_valor_ordem,
+            parameters=[params],
+        )
+
+    verificacao_captura_params = create_verificacao_captura_params(gaps=gaps)
+
+    for param in verificacao_captura_params:
+        await run_subflow(
+            env=env,
+            flow=control__jae_verificacao_captura,
+            parameters=[param],
+        )
