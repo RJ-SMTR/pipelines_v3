b5dd16ee4dc2750030bcc21a9544885af6e387a2
Duartejanaina <janainacduarte@gmail.com>
2026-07-07 18:07:10 -0300

    Altera conforme prod

--- queries/models/projeto_subsidio_sppo/staging/aux_ida_volta_circular.sql
+++ queries/models/projeto_subsidio_sppo/staging/aux_ida_volta_circular.sql
@@ -6,7 +6,10 @@
 
 select *
 from {{ ref("aux_ida_volta_circular_v1") }}
-where data not between date("2025-06-01") and date("2025-06-30")
+where
+    data < date("{{ var('DATA_SUBSIDIO_V24_INICIO') }}")
+    and data not between date("2025-06-01")
+    and date("2025-06-30")
 full outer union all by name
 select *
 from {{ ref("aux_ida_volta_circular_v2") }}
