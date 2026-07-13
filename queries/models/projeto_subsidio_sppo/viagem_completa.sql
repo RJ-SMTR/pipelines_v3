1cd43bb368be76dc00c7828fba3315da1e645a8e
Duartejanaina <janainacduarte@gmail.com>
2026-07-13 15:09:23 -0300

    Reestabelece como prod

--- queries/models/projeto_subsidio_sppo/viagem_completa.sql
+++ queries/models/projeto_subsidio_sppo/viagem_completa.sql
@@ -206,17 +206,47 @@ with
      -- shape
     filtro_desvio as (
         select
-            {% if var("run_date") > var("DATA_SUBSIDIO_V6_INICIO") %}
-                * except (rn, id_tipo_trajeto)
             {% if var("run_date") > var("DATA_SUBSIDIO_V6_INICIO") %} * except (rn)
+            {% else %} * except (rn)
             {% endif %}
-            from
+        from
+            (
+                select
+                    *,
+                    {% if var("run_date") > var("DATA_SUBSIDIO_V7_INICIO") %}
+                        -- Apuração "Madonna · The Celebration Tour in Rio"
+                        row_number() over (
+                            partition by id_veiculo, datetime_partida, datetime_chegada
+                            order by
+                                perc_conformidade_shape desc,
+                                id_tipo_trajeto,
+                                distancia_planejada desc
+                        ) as rn
+                    {% elif var("run_date") > var("DATA_SUBSIDIO_V6_INICIO") %}
+                        row_number() over (
+                            partition by id_veiculo, datetime_partida, datetime_chegada
+                            order by perc_conformidade_shape desc, id_tipo_trajeto
+                        ) as rn
+                    {% else %}
+                        row_number() over (
+                            partition by id_veiculo, datetime_partida, datetime_chegada
+                            order by perc_conformidade_shape desc
+                        ) as rn
+                    {% endif %}
+                from viagem_comp_conf
+            )
+        where rn = 1
+    ),
+    -- 4. Filtra viagens com partida ou chegada diferentes pela maior distancia
+    -- percorrida
+    filtro_partida as (
+        select * except (rn)
+        from
             (
                 select
                     *,
                     row_number() over (
                         partition by id_veiculo, datetime_partida
-                        order by distancia_planejada desc
                         order by distancia_planejada desc, id_tipo_trajeto
                     ) as rn
                 from filtro_desvio
@@ -236,3 +266,4 @@ from
             ) as rn
         from filtro_partida
     )
+where rn = 1
