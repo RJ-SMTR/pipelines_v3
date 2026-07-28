6a543d4ac9aa3a1985a76e36ec3ffb01511abf78
Guilherme Botelho <guilhermehcb@hotmail.com>
2026-07-28 17:36:54 -0300

    feat: align view_gps_onibus with conecta and zirix providers

--- queries/models/monitoramento/view_gps_onibus.sql
+++ queries/models/monitoramento/view_gps_onibus.sql
@@ -5,18 +5,16 @@
     )
 }}
 
-select *, 'sppo' as fonte_gps
-from {{ source("monitoramento", "gps_onibus_sppo") }}
+select *, 'conecta' as fonte_gps
+from {{ source("monitoramento", "gps_onibus_conecta") }}
 
 union all
 
 select *, 'zirix' as fonte_gps
-from
-    {{ source("monitoramento", "gps_onibus_zirix") }}
+from {{ source("monitoramento", "gps_onibus_zirix") }}
 
-    -- union all
-    -- select *, 'cittati' as fonte_gps
-    -- from {{ source("monitoramento", "gps_onibus_cittati") }}
-    -- union all
-    -- select *, 'conecta' as fonte_gps
-    -- from {{ source("monitoramento", "gps_onibus_conecta") }}
+union all
+
+-- union all
+-- select *, 'cittati' as fonte_gps
+-- from {{ source("monitoramento", "gps_onibus_cittati") }}
