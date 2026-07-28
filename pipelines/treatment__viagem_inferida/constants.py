576d22fe52df56dd97fbfa48aaa05cb2c22704a9
Guilherme Botelho <guilhermehcb@hotmail.com>
2026-07-28 17:36:59 -0300

    feat: add treatment__viagem_inferida materialization flow

---
+++ pipelines/treatment__viagem_inferida/constants.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Valores constantes para materialização dos dados de viagem inferida
+"""
+
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+from pipelines.common import constants as smtr_constants
+from pipelines.common.treatment.default_treatment.utils import DBTSelector
+from pipelines.treatment__gps_conecta import constants as gps_conecta_constants
+from pipelines.treatment__gps_zirix import constants as gps_zirix_constants
+from pipelines.treatment__planejamento_diario import constants as planejamento_constants
+
+VIAGEM_INFERIDA_SELECTOR = DBTSelector(
+    name="viagem_inferida",
+    initial_datetime=datetime(2025, 3, 25, 0, 0, 0, tzinfo=ZoneInfo(smtr_constants.TIMEZONE)),
+    flow_folder_name="treatment__viagem_inferida",
+    data_sources=[
+        planejamento_constants.PLANEJAMENTO_DIARIO_SELECTOR,
+        gps_conecta_constants.GPS_CONECTA_SELECTOR,
+        gps_zirix_constants.GPS_ZIRIX_SELECTOR,
+    ],
+)
