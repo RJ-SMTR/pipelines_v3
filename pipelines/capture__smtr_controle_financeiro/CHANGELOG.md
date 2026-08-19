b0ee072e06c37c6fc3b7f614739412db7cbe1c12
Rafael Pinheiro <rafael.c.pinheiro96@gmail.com>
2026-08-19 15:58:52 -0300

    cria flow controle financeiro

---
+++ pipelines/capture__smtr_controle_financeiro/CHANGELOG.md
@@ -0,0 +1,3 @@
+# Changelog - capture__smtr_controle_financeiro
+
+## [1.0.0] - yyyy-mm-dd
---
+++ pipelines/capture__smtr_controle_financeiro/constants.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""Valores contantes para captura dos dados das planilhas de controle_financeiro"""
+
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+from pipelines.common import constants as smtr_constants
+from pipelines.common.utils.gcp.bigquery import SourceTable
+
+SMTR_SOURCE_NAME = "smtr"
+
+SHEETS_BASE_URL = "https://docs.google.com/spreadsheets/d/\
+1QVfa9b8jzpQr3gac0FIlozmTaVeArtJROA343A2lMVM/export?format=csv&gid="
+
+SHEETS_CAPTURE_PARAMS = {
+    "cb": {
+        "sheet_id": "454453523",
+    },
+    "cett": {
+        "sheet_id": "0",
+    },
+}
+
+CONTROLE_FINANCEIRO_SOURCES = [
+    SourceTable(
+        source_name=SMTR_SOURCE_NAME,
+        table_id=t,
+        first_timestamp=datetime(2026, 8, 20, 0, 0, 0, tzinfo=ZoneInfo(smtr_constants.TIMEZONE)),
+        partition_date_only=True,
+        raw_filetype="csv",
+    )
+    for t in SHEETS_CAPTURE_PARAMS
+]
