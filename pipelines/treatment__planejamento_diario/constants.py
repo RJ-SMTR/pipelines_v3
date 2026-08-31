f3c31f59d07baea0b8a0eb9ce6a9a0cb51518d8c
Duartejanaina <janainacduarte@gmail.com>
2026-08-31 20:00:27 -0300

    acrescenta testes planejamento_diario

--- pipelines/treatment__planejamento_diario/constants.py
+++ pipelines/treatment__planejamento_diario/constants.py
@@ -10,18 +10,38 @@ from pipelines.common import constants as smtr_constants
 from pipelines.common.treatment.default_treatment.utils import DBTSelector, DBTTest
 
 PLANEJAMENTO_DIARIO_CHECKS_LIST = {
-    "servico_planejado_faixa_horaria": {
-        "test_consistencia_servico_planejado_faixa_horaria": {
-            "description": (
-                "Os totais de partidas e quilometragem dos serviços planejados "
-                "correspondem aos da Ordem de Serviço para cada "
-                "`feed_start_date`, `tipo_dia` e `tipo_os`."
-            )
-        },
+    "test_consistencia_servico_planejado_faixa_horaria": {
+        "description": (
+            "Os totais de partidas e quilometragem dos serviços planejados "
+            "correspondem aos da Ordem de Serviço para cada "
+            "`feed_start_date`, `tipo_dia` e `tipo_os`."
+        )
     },
     "viagem_planejada_planejamento_dia": {
         "dbt_utils__unique_combination_of_columns__viagem_planejada_planejamento_dia": {
             "description": "Todos os registros de 'data' e 'id_viagem' são únicos."
+        },
+    },
+    "sumario_faixa_servico_dia": {
+        "check_km_planejada__sumario_faixa_servico_dia": {
+            "description": "Todas as viagens possuem `km_planejada` correspondente à OS"
+        },
+    },
+    "sumario_faixa_servico_dia_pagamento": {
+        "check_km_planejada__sumario_faixa_servico_dia_pagamento": {
+            "description": "Todas as viagens possuem `km_planejada` correspondente à OS"
+        },
+    },
+    "viagem_planejada": {
+        "check_km_planejada": {
+            "description": "Todas as viagens possuem `km_planejada` correspondente à OS"
+        },
+    },
+    "viagens_remuneradas": {
+        "teto_viagens__viagens_remuneradas": {
+            "description": (
+                "Todas as viagens foram corretamente identificadas dentro das regras de limite"
+            )
         },
     },
 }
