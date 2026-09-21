1411baf2c5dbdbaa40370910490b3f89934bb362
Guilherme Botelho <guilhermehcb@hotmail.com>
2026-09-21 19:07:12 -0300

    feat: add GTFS source contract model

--- queries/models/gtfs/feed_info_gtfs.sql
+++ queries/models/gtfs/feed_info_gtfs.sql
@@ -18,23 +18,17 @@ with
             safe_cast(timestamp_captura as string) as feed_version,
             safe_cast(data_versao as date) as feed_start_date,
             null as feed_end_date,
-            safe_cast(feed_publisher_name as string) feed_publisher_name,
-            safe_cast(
-                json_value(content, '$.feed_publisher_url') as string
-            ) feed_publisher_url,
-            safe_cast(json_value(content, '$.feed_lang') as string) feed_lang,
-            safe_cast(json_value(content, '$.default_lang') as string) default_lang,
-            safe_cast(
-                json_value(content, '$.feed_contact_email') as string
-            ) feed_contact_email,
-            safe_cast(
-                json_value(content, '$.feed_contact_url') as string
-            ) feed_contact_url,
+            feed_publisher_name,
+            feed_publisher_url,
+            feed_lang,
+            default_lang,
+            feed_contact_email,
+            feed_contact_url,
             current_datetime("America/Sao_Paulo") as feed_update_datetime,
             '{{ var("version") }}' as versao_modelo
-        from {{ source("br_rj_riodejaneiro_gtfs_staging", "feed_info") }}
+        from {{ ref("staging_feed_info") }}
         {% if is_incremental() %}
-            where data_versao = '{{ var("data_versao_gtfs") }}'
+            where data_versao = date('{{ var("data_versao_gtfs") }}')
             union all
             select *
             from {{ this }}
