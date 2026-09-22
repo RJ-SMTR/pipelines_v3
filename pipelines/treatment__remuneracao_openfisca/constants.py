323050f009da65b5924e65146703eaacc4acc267
vtr363 <victor.m.rocha.vr@gmail.com>
2026-09-22 14:47:26 -0300

    atualiza constants do flow

--- pipelines/treatment__remuneracao_openfisca/constants.py
+++ pipelines/treatment__remuneracao_openfisca/constants.py
@@ -7,16 +7,10 @@ from zoneinfo import ZoneInfo
 from pipelines.common import constants as smtr_constants
 from pipelines.common.treatment.default_treatment.utils import DBTSelector
 
-# Início alinhado aos parâmetros I.9 / Tab. 2 no OpenFisca (simulação A0 jul/2026).
-REMUNERACAO_INITIAL_DATETIME = datetime(
-    2026, 7, 1, 0, 0, 0, tzinfo=ZoneInfo(smtr_constants.TIMEZONE)
-)
+ADDITIONAL_VARS = {"sistema": "rio"}
 
-# Ao reativar data_sources, definir WAIT_* aqui e COPY das pastas dos flows
-# no Dockerfile (DBTSelector lê prefect.yaml no import via flow_folder_name).
 REMUNERACAO_OPENFISCA_SELECTOR = DBTSelector(
     name="remuneracao_openfisca",
-    initial_datetime=REMUNERACAO_INITIAL_DATETIME,
-    incremental_delay_hours=5 * 24,
+    initial_datetime=datetime(2026, 7, 1, 0, 0, 0, tzinfo=ZoneInfo(smtr_constants.TIMEZONE)),
     flow_folder_name="treatment__remuneracao_openfisca",
 )
