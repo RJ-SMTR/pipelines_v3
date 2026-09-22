bf11f2b0977b8e308499380a819e65ac3813afee
Duartejanaina <janainacduarte@gmail.com>
2026-09-22 14:50:34 -0300

    Adiciona nova exceção de prazo de envio no script `veiculo_dia.sql` para o período de 1 a 15 de setembro de 2026, melhorando a captura de dados de licenciamento.

--- queries/models/monitoramento/veiculo_dia.sql
+++ queries/models/monitoramento/veiculo_dia.sql
@@ -122,6 +122,10 @@ with
                     data between "2026-08-16" and "2026-08-31"  -- Exceção devido a falha na captura dos dados de licenciamento
                     and data_processamento between "2026-08-16" and "2026-09-10"
                 )
+                or (
+                    data between "2026-09-01" and "2026-09-15"  -- Exceção devido a ausência de dados de licenciamento
+                    and data_processamento between "2026-09-01" and "2026-09-22"
+                )
             )
             {% if is_incremental() %}
                 and data between date("{{ var('date_range_start') }}") and date(
